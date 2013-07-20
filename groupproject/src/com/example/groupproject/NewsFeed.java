4412759d1af902bf0a2435a7b41ad9db99b90e30
xd6 <jmt7les@gmail.com>
2013-07-19 23:29:22 -0400

    Fixed HTML loading on 2.2 device.
    possible to-do: load images to left side of the corresponding story using <image>url</image> .

--- groupproject/src/com/example/groupproject/NewsFeed.java
+++ groupproject/src/com/example/groupproject/NewsFeed.java
@@ -8,6 +8,7 @@ import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,22 +27,22 @@ import android.util.Log;
 import android.util.Xml;
 import android.view.Menu;
 import android.webkit.WebView;
+import android.widget.ImageView;
 
 public class NewsFeed extends Activity {
 
   WebView webkit;
-  List<Item> entries = new ArrayList<Item>();
+  List<Item> entries;
   URL url;
 
   public static class Item 
   {
-    public final String title, link, description;
+    public final String description;
+    public final ImageView img = null;
 
-    Item(String title, String link, String description)
-    {
-      this.title = title;
-      this.link = link;
+    Item(String description){
       this.description = description;
+      
     }
   }
 
@@ -65,8 +66,6 @@ public class NewsFeed extends Activity {
 
 
   class XmlParser extends AsyncTask<String, Void, String>{
-
-    // public final String ns = null;
     public List<Item> parse(InputStream in) 
         throws XmlPullParserException, IOException
         {
@@ -74,24 +73,11 @@ public class NewsFeed extends Activity {
       {
         XmlPullParser parser = Xml.newPullParser();
         parser.setInput(in, null);
-        //parser.nextTag();
         return readFeed(parser);
       }
-      catch(XmlPullParserException x)
+      catch(Exception e)
       {
-
-        entries.add(new Item(x.toString(),"x","x"));
-        return entries;
-      }
-      catch(IOException x)
-      {
-
-        entries.add(new Item(x.toString(),"i","i"));
-        return entries;
-      }
-      catch (NullPointerException e)
-      {
-        entries.add(new Item(e.toString(),"i","i"));
+        entries.add(new Item("Error loading content."));
         return entries;
       }
       finally
@@ -106,64 +92,35 @@ public class NewsFeed extends Activity {
         {
       InputStream stream = null;
       List<Item> entries = null;
-      XmlParser parser = new XmlParser();
 
       try
       {
         stream = downloadUrl(url);
         entries = parse(stream);
       }
-      catch (NullPointerException e)
+      catch(Exception e)
       {
-        return e.toString();
-
-      }
-      catch (IOException e )
-      {
-        return e.toString() ;
-
-      }
-      catch ( XmlPullParserException e)
-      {
-        return e.toString() ;
-
+        return "Error loading content.";
       }
       finally
       {
         if(stream != null)
           stream.close();
-        else 
-          return "NULL STREAM";
       }
-      /*  
-      StringBuilder code = new StringBuilder();
+      StringBuilder code = new StringBuilder("<html><body>");
       for (Item item : entries)
       {
-        code.append(item.title);
+        code.append(item.description + "</br>");
       }
-      return code.toString();*/
-      if(entries.size() == 0)
-        entries.add(new Item("empty","",""));
-      return entries.get(0).title;
+      code.append("</body></html>");
+      
+      return code.toString();
         }
 
     private List<Item> readFeed(XmlPullParser parser) 
         throws XmlPullParserException, IOException
         {
       List<Item> entries = new ArrayList<Item>();
-
-      //parser.require(XmlPullParser.START_TAG,  null,  "item");
-
-      /* while (parser.next() != XmlPullParser.END_TAG)
-      {
-        if (parser.getEventType() != XmlPullParser.START_TAG)
-          continue;
-
-        String name = parser.getName();
-
-        if (name.equalsIgnoreCase("location"))
-          entries.add(readItem(parser));
-      }*/
       int eventType = 0;
 
       while (eventType != XmlPullParser.END_DOCUMENT)
@@ -171,95 +128,12 @@ public class NewsFeed extends Activity {
         eventType = parser.next();
         if (eventType == XmlPullParser.START_TAG)
           if (parser.getName().equalsIgnoreCase("description"))
-            entries.add(new Item(parser.nextText(),"",""));
+            entries.add(new Item(parser.nextText()));
 
       }
       return entries;
         }
 
-
-
-    private String readTitle(XmlPullParser parser)
-        throws XmlPullParserException, IOException
-        {
-      parser.require(XmlPullParser.START_TAG, null, "title");
-      String title = readText(parser);
-      parser.require(XmlPullParser.END_TAG,  null,  "title");
-      return title;
-        }
-    private String readLink(XmlPullParser parser)
-        throws XmlPullParserException, IOException
-        {
-      parser.require(XmlPullParser.START_TAG, null, "link");
-      String link = readText(parser);
-      parser.require(XmlPullParser.END_TAG,  null,  "link");
-      return link;
-        }
-    private String readDescription(XmlPullParser parser)
-        throws XmlPullParserException, IOException
-        {
-      parser.require(XmlPullParser.START_TAG, null, "description");
-      String description = readText(parser);
-      parser.require(XmlPullParser.END_TAG,  null,  "description");
-      return description;
-        }
-
-    private String readText(XmlPullParser parser)
-        throws XmlPullParserException, IOException
-        {
-      String result = "";
-      if (parser.next() == XmlPullParser.TEXT)
-      {
-        result = parser.getText();
-        parser.nextTag();
-      }
-      return result;
-        }
-
-    private void skip(XmlPullParser parser)
-        throws XmlPullParserException, IOException
-        {
-      if (parser.getEventType() != XmlPullParser.START_TAG)
-        throw new IllegalStateException();
-
-      int depth = 1;
-      while (depth != 0)
-      {
-        switch (parser.next())
-        {
-        case XmlPullParser.END_TAG:
-          depth--;
-          break;
-        case XmlPullParser.START_TAG:
-          depth++;
-          break;
-        }
-      }
-        }
-
-    private Item readItem(XmlPullParser parser)
-        throws XmlPullParserException, IOException
-        {
-      parser.require(XmlPullParser.START_TAG,  null, "item");
-      String title = "";
-      String link = "";
-      String description = "";
-      while (parser.next() != XmlPullParser.END_TAG)
-      {
-        if (parser.getEventType() != XmlPullParser.START_TAG)
-          continue;
-        String name = parser.getName();
-        if (name.equals("title"))
-          title = readTitle(parser);
-        else if (name.equals("link"))
-          link = readLink(parser);
-        else if (name.equals("description"))
-          description = readDescription(parser);
-        else
-          skip(parser);
-      }
-      return new Item(title, link, description);
-        }//readItem
 
     @Override
     protected String doInBackground(String... urls) {
@@ -283,7 +157,8 @@ public class NewsFeed extends Activity {
     {
       setContentView(R.layout.activity_main);
       webkit = (WebView)findViewById(R.id.webkit);
-      webkit.loadData(code,  "text/html", "UTF-8");
+      webkit.loadData(URLEncoder.encode(code).replaceAll("\\+"," "),  "text/html", "UTF-8");
+      
     }
 
     private InputStream downloadUrl(String urlString) 
@@ -303,35 +178,5 @@ public class NewsFeed extends Activity {
 
   }
 
-  public String Xml (XmlPullParser xpp) throws XmlPullParserException, IOException
-  {
-    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-    factory.setNamespaceAware(true);
-    //XmlPullParser 
-    xpp = factory.newPullParser();
-
-    xpp.setInput( new StringReader ( "<description></description>" ) );
-    int eventType = xpp.getEventType();
-    while (eventType != XmlPullParser.END_DOCUMENT) {
-      if(eventType == XmlPullParser.START_DOCUMENT) {
-        return("Start document");
-      } else if(eventType == XmlPullParser.START_TAG) {
-        if (xpp.getName().equalsIgnoreCase("description"))
-        {
-          eventType = xpp.next();
-          if (eventType == XmlPullParser.TEXT)
-            return xpp.getText();
-        }   
-        return("Start tag "+xpp.getName());
-      } else if(eventType == XmlPullParser.END_TAG) {
-        return("End tag "+xpp.getName());
-      } else if(eventType == XmlPullParser.TEXT) {
-        return("Text "+xpp.getText());
-      }
-      eventType = xpp.next();
-    }
-    return("End document");
-
-  }
 
 }
