7fc44c5f8a368e2a6bf0eb23006343a8ee17936e
xd6 <jmt7les@gmail.com>
2013-07-28 20:33:22 -0400

    Update SchoolView.java

--- groupproject/src/com/example/groupproject/SchoolView.java
+++ groupproject/src/com/example/groupproject/SchoolView.java
@@ -1,11 +1,14 @@
+
+/*Xavier Gagnon 2013*/
+
 package com.example.groupproject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,52 +21,74 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.*;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.Activity;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SchoolView extends Activity{
+public class SchoolView extends Activity {
 
   static TextView text;
-  GoogleMap map;
-  String searchText="";
+  static GoogleMap map;
+  static boolean click = false;
+  static String zipCode = "";
+
+  //this is my Google API key (for Places API) 
+  //other api key (each one has a 1k/day access limit) 
+  //    AIzaSyD-lHpxNV6YKsLkvV9Wxudlwv9nExs6TIQ
   
-  //this is my Google API key (for Places API)
-  private final String key = "AIzaSyCM_9XOwDOglYF0-p86M8mlZhLeg2gVBfk";
-  
+  private final String key = "AIzaSyCisJv4V2lpdUFlBFewuYPb4hn4gmJNNQ4";
+
   //radius within which to search
   protected static int radius = 12500;
-  
+
   protected static String 
   placesSearchString="", 
   searchType ="", 
   JSONstring="";
-  
 
   
   @Override
   protected void onCreate (Bundle bundle)
   {
-
     super.onCreate(bundle);
-    LatLng latlng = new LatLng(MainActivity.latitude, MainActivity.longitude);
 
+    setContentView(R.layout.schoolview);
+
+    text = (TextView)findViewById(R.id.mapText);
+
+    map = ((MapFragment)
+        getFragmentManager().findFragmentById(R.id.map))
+        .getMap();
+
+ }
+  
+  @Override
+  protected void onStart ()
+  {
+    super.onStart();
+    
+    LatLng latlng = new LatLng(MainActivity.latitude, MainActivity.longitude);
+    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    map.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 12));
+    
+    zipCode = getIntent().getExtras().getString("zipCode");
+    //Toast.makeText(this,  zipCode, Toast.LENGTH_SHORT).show();
+    
     //Search string
     searchType = "school";
-
+ 
     //for Places API, return JSON string with info needed
-    placesSearchString = "" +
-        "https://maps.googleapis.com/maps/api/place/textsearch/" 
+    placesSearchString = "https://maps.googleapis.com/maps/api/place/textsearch/" 
         +"json?location="
         +latlng.latitude +","+ latlng.longitude
         +"&radius=" + radius 
@@ -71,28 +96,16 @@ public class SchoolView extends Activity{
         +"&query=school"
         +"&types=" + searchType
         +"&key=" + key;
-    
-    setContentView(R.layout.schoolview);
-
-    text = (TextView)findViewById(R.id.mapText);
-
-     map = ((MapFragment)
-        getFragmentManager().findFragmentById(R.id.map))
-        .getMap();
-    // if (map == null)return;
-    map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-    map.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 11));
-    
     new PlacesSearch().execute(placesSearchString);
   }
 
-
   //JSON parsing class for Places API
-  class PlacesSearch extends AsyncTask<String, Void, List<POI>>{
+  class PlacesSearch extends AsyncTask<String, Void, List<POI> >{
 
     @Override
     protected List<POI> doInBackground(String... urls) {
 
+      
       //these POI's will be used to add markers to the map later on
       List<POI>markers = new ArrayList<POI>();
       String 
@@ -105,72 +118,79 @@ public class SchoolView extends Activity{
       JSONArray jArray = null;
       JSONObject json = null;
 
-        json = getJSONFromUrl(urls[0]);
-        try{
-          jArray = json.getJSONArray("results");
+      json = getJSONFromUrl(urls[0]);
+      try{
+        jArray = json.getJSONArray("results");
 
-          for (int i = 0; i < jArray.length(); i++)
-          {
-            JSONObject o = jArray.getJSONObject(i);
-            JSONObject geom = o.getJSONObject("geometry");
-            //location is within the geometry element
-            JSONObject location = geom.getJSONObject("location");
-
-            icon = o.getString("formatted_address");
-            lat  = location.getString("lat");
-            lng  = location.getString("lng");
-            
-            name = o.getString("name");
-
-            //add the new POI to the list
-            markers.add(new POI(lat, lng, name, icon));
-          }
-        }
-        catch(JSONException e)
+        for (int i = 0; i < jArray.length(); i++)
         {
-          markers.add(new POI("0", "0", e.getLocalizedMessage(), icon));
+          JSONObject o = jArray.getJSONObject(i);
+          JSONObject geom = o.getJSONObject("geometry");
+          //location is within the geometry element
+          JSONObject location = geom.getJSONObject("location");
+
+          icon = o.getString("formatted_address");
+          lat  = location.getString("lat");
+          lng  = location.getString("lng");
+
+          name = o.getString("name");
+
+          
+          //add the new POI to the list
+          markers.add(new POI(lat, lng, name, icon));
         }
-      
+      }
+      catch(JSONException e)
+      {
+        markers.add(new POI("0", "0", e.getLocalizedMessage(), icon));
+      }
       return markers;
     }
 
     @Override
     protected void onPostExecute(List<POI> markerList) {
-      
-   BitmapDescriptor bmp;
-   Marker mark;
-     for (int i = 0; i < markerList.size(); i++)
+
+      for (int i = 0; i < markerList.size(); i++)
       {
 
-       POI poi = markerList.get(i);
-      mark = map.addMarker(new MarkerOptions()
-      .position(new LatLng(Double.valueOf(poi.lat),
-                           Double.valueOf(poi.lng)))
-                  .title(poi.name)
-                  .snippet(poi.icon));
-      if (i < 1){
-        map.animateCamera(CameraUpdateFactory
-            .newLatLngZoom(new LatLng(Double.parseDouble(poi.lat), 
-                                      Double.parseDouble(poi.lng)),
-             10));
-      }
-      
-      try{
-        bmp = BitmapDescriptorFactory.fromBitmap(
-            BitmapFactory.decodeStream(
-                downloadUrl(poi.icon)));
-      }
-      catch (Exception e)
-      {
-        bmp = BitmapDescriptorFactory.defaultMarker();
-      }
-      mark.setIcon(bmp);
-      text.setText("Results found: " + i+1);
-      
-       }
-     
+        POI poi = markerList.get(i);
+        map.addMarker(new MarkerOptions()
+        .position(new LatLng(Double.valueOf(poi.lat),
+            Double.valueOf(poi.lng)))
+            .title(poi.name)
+            .snippet(poi.icon));
 
+
+        //zoom in on the first location returned 
+        if (i == markerList.size()){
+          map.animateCamera(CameraUpdateFactory
+              .newLatLngZoom(new LatLng(Double.parseDouble(poi.lat), 
+                  Double.parseDouble(poi.lng)),
+                  10));
+
+        }
+        text.setText("Showing schools near: " + zipCode);
+
+      }
       
+      map.setOnInfoWindowClickListener(new OnInfoWindowClickListener(){
+
+        @Override
+        public void onInfoWindowClick(Marker marker) {
+          String url = marker.getTitle() + " " + zipCode;
+          //url.replaceAll("\\s","+");
+          try {
+            url = URLEncoder.encode(url, "UTF-8");
+          } catch (UnsupportedEncodingException e) {
+            url="";
+          }
+          url = "http://google.com/search?&q=" + url;
+          Intent intent = new Intent(Intent.ACTION_VIEW);
+          intent.setData(Uri.parse(url));
+          startActivity(intent);
+        }
+        
+      });
     }
 
 
@@ -219,25 +239,11 @@ public class SchoolView extends Activity{
       { placesBuilder.append("Error loading data");}
       return placesBuilder.toString();
     }
-    
-    private InputStream downloadUrl(String urlString) 
-        throws IOException
-        {
-      URL url = new URL(urlString);
-      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-      conn.setReadTimeout(10000 );
-      conn.setConnectTimeout(15000);
-      conn.setRequestMethod("GET");
-      conn.setDoInput(true);
-      conn.connect();
-      return conn.getInputStream();
-        }
 
   }
 
   class POI  
   {
-    //String icon, name, rating, lat, lng, reference;
     String lat, lng, name, icon;
 
     POI(String lat2, String lng2, String name2, String icon2)
